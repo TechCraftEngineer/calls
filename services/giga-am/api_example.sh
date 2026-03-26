51bca3a4d8e19bab37d6b6b8cc3f1bd56644d0ba
Maksim <kodermax@gmail.com>
2026-03-26 22:32:54 +0300

    up

--- services/giga-am/api_example.sh
+++ services/giga-am/api_example.sh
@@ -3,7 +3,7 @@
 # Пример использования API через curl
 
 # URL вашего Hugging Face Space
-API_URL="https://kodermax-giga-am.hf.space"
+API_URL="https://vnggncb-giga-am.hf.space"
 
 # 1. Проверка работоспособности
 echo "1. Проверка статуса API..."
